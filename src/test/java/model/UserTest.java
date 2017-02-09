a270966d60466b35032aba259cfa2ccce678038e
tabher <tab.her@concordusa.com>
2017-02-09 16:05:34 -0600

    bad unit test

--- src/test/java/model/UserTest.java
+++ src/test/java/model/UserTest.java
@@ -2,6 +2,9 @@ package model;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -9,13 +12,22 @@ import static org.junit.Assert.*;
  */
 public class UserTest
 {
-    User user = new User("Tab", "Her", "1007 Reaney Ave");
+    User user = new User("Tabb", "Her", "1007 Reaney Ave");
+    List<User> enlistedUsers = new ArrayList<User>();
 
     @Test
     public void testGetUserName()
     {
         //assert Username will be string "Tab"
         assertEquals("Tab", user.getFirstName());
+    }
+
+    @Test
+    public void testGetUserArray()
+    {
+
+        //assert will return array of 2 people
+        assertEquals(2, enlistedUsers.size());
     }
 
 }
